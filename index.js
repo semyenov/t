b08b255f0fba999361f6c6431709428745968667
Alexander Semyenov <semyenov@hotmail.com>
2024-04-30 10:47:15 +0300

    .

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-import App from "./lib/app.js";
+import blessed from "blessed";
+
 import NetworkManager from "./lib/network.js";
 import Store from "./lib/store.js";
+import App from "./lib/app.js";
 
-const key = process.argv[2];
-const network = new NetworkManager(key);
+async function init() {
+  const key = process.argv[2];
+  if (!key) {
+    throw new Error("Usage: node index.js <key>");
+  }
 
-const store = new Store();
-const app = new App(store, network);
+  const program = blessed.program({
+    smartCSR: true,
+    fullUnicode: true,
+    autoPadding: true,
+    terminal: "xterm-256color",
+    log: "./debug.log",
+    debug: true,
+    dump: true,
+  });
+  const screen = blessed.screen({
+    program: program,
+    useBCE: true,
+    title: "Task Manager with Hyperswarm",
+    cursor: {
+      artificial: true,
+      shape: 'underline',
+      blink: true,
+      color: null
+    }
+  })
+
+  const network = new NetworkManager(key);
+  const store = new Store();
+  const app = new App(
+    screen,
+    network,
+    store,
+  );
+
+  return app;
+}
+
+init()
