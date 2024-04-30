a68b8c98e596807a1c03d5b64a092e85458cd657
Alexander Semyenov <semyenov@hotmail.com>
2024-04-30 09:31:49 +0300

    good

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 import App from "./lib/app.js";
+import NetworkManager from "./lib/network.js";
 import Store from "./lib/store.js";
 
+const key = process.argv[2];
+const network = new NetworkManager(key);
+
 const store = new Store();
-const renderer = new App(store);
-
-// store.on("tasksUpdated", (tasks) => {
-//     renderer.updateTasks(tasks);
-// });
-
-// import Network from './lib/network.js';
-//
-// const mediator = new Mediator();
-// const taskManager = new TaskManager(mediator);
-// const network = new Network(mediator);
-
-// // Подписываемся на события от Network через Mediator
-// mediator.on('tasksUpdated', taskManager.updateTasks.bind(taskManager));
-
-// // Подписываемся на события от TaskManager через Mediator и Network
-// network.on('addTask', taskManager.addTask.bind(taskManager));
+const app = new App(store, network);
